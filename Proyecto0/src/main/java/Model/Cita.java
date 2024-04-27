0e0555f68bd019d8576d766c67aa0dcdb037692c
sam <samiandresduma@gmail.com>
2024-04-26 19:06:49 -0600

    Cita y Servicio
    Cita y servicios arreglados

--- Proyecto0/src/main/java/Model/Cita.java
+++ Proyecto0/src/main/java/Model/Cita.java
@@ -2,6 +2,17 @@ package Model;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 
 public class Cita {
@@ -9,10 +20,34 @@ public class Cita {
     private int numero;
     private LocalDateTime fecha;
     private boolean estado;
+    Cliente cliente = null;
+    Servicio servicio;
+    private String emailFrom = "barberialosmechudos@gmail.com";
+    private String passwordFrom = "uhpl cxnl eqsz hyrb";
+    private String emailTo;
+    private String subject = "CONFIRMAR VISITA";
+    private String content = "Saludos estimado cliente de Barberia Los Mechudos, "
+            + "por este medio le indicamos que debe comunicarse lo más antes "
+            + "posible con nosotros para poder confirmar su cita, "
+            + "la fecha límite de confirmación es hoy antes de las 11:59pm, un saludo!";
+    
+    
+    private Properties mProperties;
+    private Session mSession;
+    private MimeMessage mCorreo;
 
-    public Cita(LocalDateTime fecha) {
+    public Cita(LocalDateTime fecha, Cliente cliente, Servicio servicio) {
         this.fecha = fecha;
+        this.cliente = cliente;
+        this.estado = false;
+        this.numero = consecutivo;
+        this.servicio = servicio;
+        this.emailTo = cliente.getEmail();
+        consecutivo++;
+        mProperties = new Properties();
+        
     }
+    
 
     public int getNumero() {
         return numero;
@@ -22,12 +57,16 @@ public class Cita {
         return fecha;
     }
 
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+    
     public boolean isEstado() {
         return estado;
     }
-
-    public void setFecha(LocalDateTime fecha) {
-        this.fecha = fecha;
+    
+    public void setEstado(boolean estado) {
+        this.estado = estado;
     }
     
     public DayOfWeek getDia(){
@@ -44,12 +83,51 @@ public class Cita {
         this.fecha.withHour(hora);
     }
 
-    public void setEstado(boolean estado) {
-        this.estado = estado;
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio) {
+        this.servicio = servicio;
     }
     
+    
     public void confirmar(){
+        mProperties.put("mail.smtp.host", "smtp.gmail.com");
+        mProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        mProperties.setProperty("mail.smtp.starttls.enable", "true");
+        mProperties.setProperty("mail.smtp.port", "587");
+        mProperties.setProperty("mail.smtp.user", emailFrom);
+        mProperties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+        mProperties.setProperty("mail.smtp.auth", "true");
         
+        mSession = Session.getDefaultInstance(mProperties);
+        try {
+            mCorreo = new MimeMessage(mSession);
+            mCorreo.setFrom(new InternetAddress(emailFrom));
+            mCorreo.setRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
+            mCorreo.setSubject(subject);
+            mCorreo.setText(content, "ISO-8859-1", "html");
+            
+            
+        } catch (AddressException ex) {
+            Logger.getLogger(Cita.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MessagingException ex) {
+            Logger.getLogger(Cita.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        try {
+            Transport mTransport = mSession.getTransport("smtp");
+            mTransport.connect(emailFrom, passwordFrom);
+            mTransport.sendMessage(mCorreo, mCorreo.getRecipients(Message.RecipientType.TO));
+            mTransport.close();
+            System.out.println("Correo enviado");
+        } catch (NoSuchProviderException ex) {
+            Logger.getLogger(Cita.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MessagingException ex) {
+            Logger.getLogger(Cita.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
---
+++ Proyecto0/src/main/java/Model/Servicio.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author samia
+ */
+public class Servicio {
+    String tipo;
+    String descripcion;
+
+    public Servicio(String tipo, String descripcion) {
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Servicio{" + "tipo=" + tipo + ", descripcion=" + descripcion + '}';
+    }
+    
+    
+}
